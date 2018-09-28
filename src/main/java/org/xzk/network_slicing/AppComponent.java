275be351cba6340862390ed3d43d9d64942b672c
Xzk Khooi <khooi8913@gmail.com>
2018-09-28 11:30:33 +0800

    Updated AppComponent

--- src/main/java/org/xzk/network_slicing/AppComponent.java
+++ src/main/java/org/xzk/network_slicing/AppComponent.java
@@ -15,28 +15,81 @@
  */
 package org.xzk.network_slicing;
 
-import org.apache.felix.scr.annotations.Activate;
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.*;
+import org.onlab.packet.Ethernet;
+import org.onosproject.core.ApplicationId;
+import org.onosproject.core.CoreService;
+import org.onosproject.net.flow.DefaultTrafficSelector;
+import org.onosproject.net.flow.FlowRuleService;
+import org.onosproject.net.flow.TrafficSelector;
+import org.onosproject.net.flowobjective.FlowObjectiveService;
+import org.onosproject.net.host.HostService;
+import org.onosproject.net.packet.PacketContext;
+import org.onosproject.net.packet.PacketPriority;
+import org.onosproject.net.packet.PacketProcessor;
+import org.onosproject.net.packet.PacketService;
+import org.onosproject.net.topology.TopologyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Skeletal ONOS application component.
- */
 @Component(immediate = true)
 public class AppComponent {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
+    private ApplicationId appId;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected CoreService coreService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected PacketService packetService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected TopologyService topologyService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected HostService hostService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected FlowObjectiveService flowObjectiveService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected FlowRuleService flowRuleService;
+
     @Activate
     protected void activate() {
+        appId = coreService.registerApplication("org.xzk.network_slicing");
         log.info("Started");
+    }
+
+    // Request packet in via packet service
+    private void requestIntercepts() {
+        TrafficSelector.Builder trafficSelector = DefaultTrafficSelector.builder();
+        trafficSelector.matchEthType(Ethernet.TYPE_IPV4);
+        trafficSelector.matchEthType(Ethernet.TYPE_ARP);
+        packetService.requestPackets(trafficSelector.build(), PacketPriority.REACTIVE, appId);
+    }
+
+    // To cancel request for packet in via packet service
+    private void withdrawIntercepts() {
+        TrafficSelector.Builder trafficSelector = DefaultTrafficSelector.builder();
+        trafficSelector.matchEthType(Ethernet.TYPE_IPV4);
+        trafficSelector.matchEthType(Ethernet.TYPE_ARP);
+        packetService.cancelPackets(trafficSelector.build(), PacketPriority.REACTIVE, appId);
     }
 
     @Deactivate
     protected void deactivate() {
         log.info("Stopped");
+    }
+
+    private class VirtualNetworkPacketProcessor implements PacketProcessor {
+
+        @Override
+        public void process(PacketContext packetContext) {
+            // TODO
+        }
     }
 
 }
